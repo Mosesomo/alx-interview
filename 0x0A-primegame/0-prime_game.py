fe08bc95b74e3792ea8155b12b018d17f00405bc
Mosesomo <moseswamboga4@gmail.com>
2024-05-17 21:00:23 +0300

    case 1000

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,16 +1,19 @@
-#!/usr/bin/python3
-'''Prime Game'''
-
-
 def isWinner(x, nums):
-    '''finds the winner'''
+    '''Determines the overall winner after x rounds.'''
+    if x < 1 or not nums:
+        return None
+    
+    # Calculate primes up to the maximum number in nums
+    max_num = max(nums)
+    primes = sieve_of_eratosthenes(max_num)
+    
     winnerCounter = {'Maria': 0, 'Ben': 0}
 
-    for i in range(x):
-        roundWinner = isRoundWinner(nums[i], x)
-        if roundWinner is not None:
-            winnerCounter[roundWinner] += 1
-
+    for num in nums:
+        round_winner = play_round(num, primes)
+        if round_winner:
+            winnerCounter[round_winner] += 1
+    
     if winnerCounter['Maria'] > winnerCounter['Ben']:
         return 'Maria'
     elif winnerCounter['Ben'] > winnerCounter['Maria']:
@@ -18,40 +21,43 @@ def isWinner(x, nums):
     else:
         return None
 
+def sieve_of_eratosthenes(n):
+    '''Returns a list of booleans indicating prime status up to n.'''
+    is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False  # 0 and 1 are not prime numbers
+    
+    for start in range(2, int(n**0.5) + 1):
+        if is_prime[start]:
+            for multiple in range(start * start, n + 1, start):
+                is_prime[multiple] = False
+    
+    return is_prime
 
-def isRoundWinner(n, x):
-    '''find round winner'''
-    list = [i for i in range(1, n + 1)]
+def play_round(n, primes):
+    '''Plays a single round and returns the winner.'''
+    list_nums = list(range(1, n + 1))
     players = ['Maria', 'Ben']
+    current_player = 0  # Maria starts first
 
-    for i in range(n):
-        currentPlayer = players[i % 2]
-        selectedIdxs = []
-        prime = -1
-        for idx, num in enumerate(list):
-            if prime != -1:
-                if num % prime == 0:
-                    selectedIdxs.append(idx)
-            else:
-                if isPrime(num):
-                    selectedIdxs.append(idx)
-                    prime = num
-        if prime == -1:
-            if currentPlayer == players[0]:
-                return players[1]
-            else:
-                return players[0]
-        else:
-            for idx, val in enumerate(selectedIdxs):
-                del list[val - idx]
-    return None
+    # To keep track of which numbers have been removed
+    removed = [False] * (n + 1)
 
+    while True:
+        # Find the smallest prime number still in the list
+        prime_to_remove = -1
+        for i in range(1, n + 1):
+            if primes[i] and not removed[i]:
+                prime_to_remove = i
+                break
 
-def isPrime(n):
-    if n == 1 or n == 0 or (n % 2 == 0 and n > 2):
-        return False
-    else:
-        for i in range(3, int(n**(1/2))+1, 2):
-            if n % i == 0:
-                return "Not prime"
-        return True
+        # If no prime number can be found, current player loses
+        if prime_to_remove == -1:
+            return players[1 - current_player]
+        
+        # Remove the prime number and all its multiples
+        for i in range(prime_to_remove, n + 1, prime_to_remove):
+            removed[i] = True
+        
+        # Switch players
+        current_player = 1 - current_player
+
